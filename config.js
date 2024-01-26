b524aa4a7450648d5493e818ea87a5e053926c2e
MYTllc <89267217+MYTllc@users.noreply.github.com>
2024-01-26 14:51:52 +0300

    Update config.js
    V1

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
 token: process.env.TOKEN,
-  prefix: '+', // Command prefix
+  prefix: '&', // Command prefix
   voiceChannelId: '1198320859148992673',
-  logChannelId: '1188948553360478289', // Custom channel ID for the log
-  requiredRoleId: '1130201746610856068', // ID of the required role
+  logChannelId: '1163576791785541732', // Custom channel ID for the log
+  requiredRoleId: '1163959965455745125', // ID of the required role
   dbFilePath: 'giveaways.db', // SQLite database file path
-  yourImageURL: 'https://cdn.discordapp.com/attachments/1150031753097003138/1182730280709140521/giveaway-bot-banner-mov-te.gif?ex=6585c268&is=65734d68&hm=a6acae50a560f4029a837cf433bd34cbe4475ff2593fa0b53cd061f742df9b74&', // Replace with your image URL
+  yourImageURL: 'https://media.discordapp.net/attachments/1080519797906886656/1182087700401102858/te-banner-final-v1.gif?ex=65c40575&is=65b19075&hm=9df347edf1f88d219868822708385764f34b4d7b61c8d167d46de77e12d3a821&', // Replace with your image URL
 };
 
 
